4e43b6453d7a1c564dd677d4c0bea1f2d61c4d4c
nourhan031 <nourhanabdallah031@gmail.com>
2024-04-13 19:41:18 +0200

    Added css for the Home page

---
+++ style/scripts/dropdown.js
@@ -0,0 +1,9 @@
+// Add JavaScript code to show/hide the dropdown menu
+
+// document.getElementById('explore').addEventListener('mouseover', function() {
+//     document.getElementById('dropdown').style.display = 'block';
+// });
+
+// document.getElementById('explore').addEventListener('mouseout', function() {
+//     document.getElementById('dropdown').style.display = 'none';
+// });
