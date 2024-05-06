f9290e6664a38e39690d9a00aa67a1b5cf051573
Mahmoud Zaghloul <91810603+mahmoudz251@users.noreply.github.com>
2024-05-06 15:31:03 +0300

    function Add recipe (in progress)

--- static/scripts/data.js
+++ static/scripts/data.js
@@ -412,10 +412,47 @@ let add = document.getElementById("ptn_add");
 function Add_recipe() {
   let recipe_name = document.getElementById('recipeName');
   let ing = document.getElementById('ingredients');
+  let des = document.getElementById('description');
+  let arr = [];
+  let word = "";
+  let arr2 = [];
+
+
+
+
+  let idx = 0;
+  while (ing.value[idx] != '.') {
+    if (ing.value[idx] == ';') {
+      arr.push(word);
+      word = "";
+      idx++;
+      continue;
+    }
+
+    if (!(ing.value[idx] == ' ' && word.length == 0)) {
+      let new_word = word.concat(ing.value[idx]);
+      word = new_word;
+    }
+    idx++;
+  }
+
+  word = "";
   
-  
- 
-  
+  idx = 0;
+  while(des.value[idx] != '.') {
+    if (des.value[idx] == ';') {
+      arr2.push(word);
+      word = "";
+      idx++;
+      continue;
+    }
+
+    if (!(des.value[idx] == ' ' && word.length == 0)) {
+      let new_word = word.concat(des.value[idx]);
+      word = new_word;
+    }
+    idx++;
+  }
 
 
   const recipeData = {
@@ -424,8 +461,8 @@ function Add_recipe() {
     imageAlt: recipe_name.value,
     recipeName: recipe_name.value,
     id: incId(),
-    ingredients: [],
-    directions: []
+    ingredients: arr,
+    directions: arr2
 
   };
 
@@ -444,7 +481,7 @@ function hello() {
 
 }
 
-add.onclick = function() {
+add.onclick = function () {
   Add_recipe();
 }
 
